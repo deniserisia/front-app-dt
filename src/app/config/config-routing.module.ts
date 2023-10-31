04c43f73e932fcaf6a5169af4f12f29d31b65a90
DeniseRisia <risiaresponde@gmail.com>
2023-10-31 14:39:53 -0300

    add page home e meu perfil

---
+++ src/app/config/config-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from '../layout/layout.component';
+import { MeuPerfilComponent } from './meu-perfil/meu-perfil.component';
+
+const routes: Routes = [
+  {path:'configurações', component: LayoutComponent, children:[
+    {path:'meu-perfil', component:MeuPerfilComponent}
+  ]}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ConfigRoutingModule { }
