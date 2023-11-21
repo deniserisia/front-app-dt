19ba2c3ce99a3a327b054afc4d95e27697f4addb
DeniseRisia <risiaresponde@gmail.com>
2023-11-21 12:43:12 -0300

    add dos cards do relatorio

--- src/app/servicos/relatorios/relatorios.component.ts
+++ src/app/servicos/relatorios/relatorios.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as html2pdf from 'html2pdf.js';
+
 
 @Component({
   selector: 'app-relatorios',
   templateUrl: './relatorios.component.html',
   styleUrls: ['./relatorios.component.css']
 })
-export class RelatoriosComponent {
+export class RelatoriosComponent implements OnInit {
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  downloadPDF(): void {
+    const content: Element = document.getElementById('pdf-content')!; // ID da div que contém o conteúdo para o PDF
+
+    const options = {
+      margin: 10,
+      filename: 'relatorio.pdf',
+      image: { type: 'jpeg', quality: 0.98 },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+    };
+
+    (html2pdf as any)().from(content).set(options).outputPdf();
+  }
 }
