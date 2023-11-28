ee9ec7c719a085258dcd3e47b3a522d3a6b32e0a
DeniseRisia <risiaresponde@gmail.com>
2023-11-28 18:30:34 -0300

    add urls

--- src/environment/environment.dev.ts
+++ src/environment/environment.dev.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     apiURLBase: 'http://localhost:8084',
-//    clientId: 'front-app',
-//    clientSecret: '@321',
+    clientId: 'front-app',
+    clientSecret: '@321',
 //    obterTokenUrl: '/oauth/token'
 };
---
+++ src/environment/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    apiURLBase: 'http://localhost:8084',
+    clientId: 'front-app-dt',
+    clientSecret: '@321',
+//    obterTokenUrl: '/oauth/token'
+};
